84545266a417645a03b30b958c6fd7a59c6a659a
paldier <paldier@hotmail.com>
2019-03-26 11:28:08 +0800

    update ddnspod

--- ddnspod/config.json.js
+++ ddnspod/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"0.1.6",
-"md5":"977ff1b340ec0a4583aab29f638e0ea6",
+"md5":"333cb2798401a77f18428427c9ced25c",
 "home_url":"Module_ddnspod.asp",
 "title":"DDnspod",
 "description":"使用Dnspod的ddns服务",
-"build_date":"2019-03-11_23:45:27"
+"build_date":"2019-03-26_11:27:56"
 }
--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -161,10 +161,10 @@
             "version": "0.0.9"
         }, 
         {
-            "build_date": "2019-03-11_23:45:27", 
+            "build_date": "2019-03-26_11:27:56", 
             "description": "使用Dnspod的ddns服务", 
             "home_url": "Module_ddnspod.asp", 
-            "md5": "977ff1b340ec0a4583aab29f638e0ea6", 
+            "md5": "333cb2798401a77f18428427c9ced25c", 
             "name": "ddnspod", 
             "tar_url": "ddnspod/ddnspod.tar.gz", 
             "title": "DDnspod", 
