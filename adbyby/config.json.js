011b17344180b1459db200cdcd5ddab6dcd82281
ZhaoWei Xu <paldier@hotmail.com>
2023-02-19 17:14:04 +0800

    repack adbyby

--- adbyby/config.json.js
+++ adbyby/config.json.js
@@ -1,9 +1,9 @@
 {
 "version":"1.3",
-"md5":"2fca5e3f665749b74d84ec29c78a7598",
+"md5":"489af49ba626092f5a7c467453cc2fc2",
 "arch":"arm",
 "home_url":"Module_adbyby.asp",
 "title":"广告屏蔽大师 Plus",
 "description":"广告屏蔽大师 Plus 可以全面过滤各种横幅、弹窗、视频广告，同时阻止跟踪、隐私窃取及各种恶意网站",
-"build_date":"2023-02-19_17:07:58"
+"build_date":"2023-02-19_17:13:59"
 }
--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -224,10 +224,10 @@
         }, 
         {
             "arch": "arm", 
-            "build_date": "2023-02-19_17:07:58", 
+            "build_date": "2023-02-19_17:13:59", 
             "description": "广告屏蔽大师 Plus 可以全面过滤各种横幅、弹窗、视频广告，同时阻止跟踪、隐私窃取及各种恶意网站", 
             "home_url": "Module_adbyby.asp", 
-            "md5": "2fca5e3f665749b74d84ec29c78a7598", 
+            "md5": "489af49ba626092f5a7c467453cc2fc2", 
             "name": "adbyby", 
             "tar_url": "adbyby/adbyby.tar.gz", 
             "title": "广告屏蔽大师 Plus", 
