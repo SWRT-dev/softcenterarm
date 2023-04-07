ddf459717a69bd6b75d6ac0653d48b75e7a9879b
ZhaoWei Xu <paldier@hotmail.com>
2023-04-07 18:41:53 +0800

    update push message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,11 +1,11 @@
 {
 "title":"<em>软件中心&nbsp;for&nbsp;"+db_softcenter_["softcenter_arch"]+"&nbsp;platform</em>",
-"content1":"<b>软件中心：1.4.8已发布，软件中心停止支持api1.1,固件版本低于5.2.1勿更新</b>",
-"content2":"固件5.2.1:已发布,<font color='#1E90FF'>修复软件中心cgi</font>",
+"content1":"<b>软件中心：1.4.9已发布，软件中心停止支持api1.1,固件版本低于5.2.2勿更新</b>",
+"content2":"固件5.2.9:已发布,<font color='#1E90FF'>修复软件中心cgi</font>",
 "content3":"<font color='#1E90FF'>交流反馈:&nbsp;&nbsp;</font>\
 <a href='https://github.com/SWRT-dev/softcenter' target='_blank'><em>1.软件中心GitHub项目</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://t.me/merlinchat' target='_blank'><em>2.加入telegram群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://www.right.com.cn/forum/forum-173-1.html' target='_blank'><em>3.恩山论坛插件版块</em></a>\
 &nbsp;&nbsp;&nbsp;&nbsp;<a href='https://jq.qq.com/?_wv=1027&k=m6D9uDVN' target='_blank'><em>4.QQ①群：1071315853</em></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='https://jq.qq.com/?_wv=1027&k=9e2KGHtf' target='_blank'><em>QQ付费vip群：692826448</em></a>",
-"content4":"<font color='#1E90FF'>当前固件版本:</font><b>"+db_softcenter_["softcenter_firmware_version"]+"</b>&nbsp;&nbsp;<font color='#1E90FF'>最新固件版本:</font><b>5.2.1</b>&nbsp;&nbsp;<a href='https://blog.paldier.com/categories/firmware/' target='_blank'><em>下载链接</em></a>"
+"content4":"<font color='#1E90FF'>当前固件版本:</font><b>"+db_softcenter_["softcenter_firmware_version"]+"</b>&nbsp;&nbsp;<font color='#1E90FF'>最新固件版本:</font><b>5.2.2</b>&nbsp;&nbsp;<a href='https://blog.paldier.com/categories/firmware/' target='_blank'><em>下载链接</em></a>"
 }
