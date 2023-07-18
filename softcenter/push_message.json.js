6f9d0779998bae04528578a36bb14d93e5d9fff5
ZhaoWei Xu <paldier@hotmail.com>
2023-07-18 19:04:15 +0800

    update push message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -2,11 +2,11 @@
 "title":"<em>软件中心&nbsp;for&nbsp;"+db_softcenter_["softcenter_arch"]+"&nbsp;platform</em>",
 "content1":"<b>软件中心：1.4.9已发布，软件中心停止支持api1.1,固件版本低于5.2.2勿更新</b>",
 "content2":"<em>📢</em>RT-AX86U PRO 领券1499元,详询客服 🛒 <a href='https://s.click.taobao.com/24VqaFu' target='_blank'><em>点此链接购买</em></a>&nbsp;&nbsp;\
-<em>📢</em>RT-AX82U 618到手价489元,详询客服 🛒 <a href='https://item.taobao.com/item.htm?id=623516166929' target='_blank'><em>点此链接购买</em></a>",
+<em>📢</em>RT-AX82U 到手价489元,详询客服 🛒 <a href='https://item.taobao.com/item.htm?id=623516166929' target='_blank'><em>点此链接购买</em></a>",
 "content3":"<font color='#1E90FF'>交流反馈:&nbsp;&nbsp;</font>\
 <a href='https://github.com/SWRT-dev/softcenter' target='_blank'><em>1.软件中心GitHub项目</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://t.me/merlinchat' target='_blank'><em>2.加入telegram群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://www.right.com.cn/forum/forum-173-1.html' target='_blank'><em>3.恩山论坛插件版块</em></a>\
 &nbsp;&nbsp;&nbsp;&nbsp;<a href='https://jq.qq.com/?_wv=1027&k=m6D9uDVN' target='_blank'><em>4.QQ①群：1071315853</em></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='https://jq.qq.com/?_wv=1027&k=9e2KGHtf' target='_blank'><em>QQ付费vip群：692826448</em></a>",
-"content4":"<font color='#1E90FF'>当前固件版本:</font><b>"+db_softcenter_["softcenter_firmware_version"]+"</b>&nbsp;&nbsp;<font color='#1E90FF'>最新固件版本:</font><b>5.2.2</b>&nbsp;&nbsp;<a href='https://blog.paldier.com/categories/firmware/' target='_blank'><em>下载链接</em></a>"
+"content4":"<font color='#1E90FF'>当前固件版本:</font><b>"+db_softcenter_["softcenter_firmware_version"]+"</b>&nbsp;&nbsp;<font color='#1E90FF'>最新固件版本:</font><b>5.2.3</b>&nbsp;&nbsp;<a href='https://blog.paldier.com/categories/firmware/' target='_blank'><em>下载链接</em></a>"
 }
--- softcenter/push_message_en.json.js
+++ softcenter/push_message_en.json.js
@@ -1,11 +1,11 @@
 {
 "title":"<em>Software Center&nbsp;for&nbsp;"+db_softcenter_["softcenter_arch"]+"&nbsp;platform</em>",
 "content1":"<b>Software Center: </b>1.4.9,<font color='#1E90FF'>&nbsp;softcenter dropped support for api1.1, please do not update when the firmware version is lower than 5.2.2</font>",
-"content2":"New firmware 5.2.2 has been released: fix softcenter cgi",
+"content2":"New firmware 5.2.3 has been released: <a href='https://www.swrt.site/Changelog' target='_blank'><em>Changelog</em></a>",
 "content3":"<font color='#1E90FF'>Feedback:&nbsp;&nbsp;</font>\
 <a href='https://github.com/SWRT-dev/softcenter' target='_blank'><em>1.GitHub</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://t.me/merlinchat' target='_blank'><em>2.Join telegram</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://www.right.com.cn/forum/forum-173-1.html' target='_blank'><em>3.Chinese forum</em></a>",
-"content4":"<font color='#1E90FF'>Firmware version:</font><b>"+db_softcenter_["softcenter_firmware_version"]+"</b>&nbsp;&nbsp;<font color='#1E90FF'>Latest version:</font><b>5.2.2</b>&nbsp;&nbsp;<a href='https://github.com/SWRT-dev/Supported_Devices/blob/master/Firmware_Download.md' target='_blank'><em>Download</em></a>"
+"content4":"<font color='#1E90FF'>Firmware version:</font><b>"+db_softcenter_["softcenter_firmware_version"]+"</b>&nbsp;&nbsp;<font color='#1E90FF'>Latest version:</font><b>5.2.3</b>&nbsp;&nbsp;<a href='https://www.swrt.site/Firmware_Download' target='_blank'><em>Download</em></a>"
 }
 
