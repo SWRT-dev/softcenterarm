b7264042286efa363dad40ebc2b8db112ee58029
ZhaoWei Xu <paldier@hotmail.com>
2023-10-23 13:02:50 +0800

    update push_message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -8,5 +8,5 @@
 <a href='https://t.me/merlinchat' target='_blank'><em>2.加入telegram群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://www.right.com.cn/forum/forum-173-1.html' target='_blank'><em>3.恩山论坛插件版块</em></a>\
 &nbsp;&nbsp;&nbsp;&nbsp;<a href='https://jq.qq.com/?_wv=1027&k=m6D9uDVN' target='_blank'><em>4.QQ①群：1071315853</em></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='https://jq.qq.com/?_wv=1027&k=9e2KGHtf' target='_blank'><em>QQ付费vip群：692826448</em></a>",
-"content4":"<font color='#1E90FF'>当前固件版本:</font><b>"+db_softcenter_["softcenter_firmware_version"]+"</b>&nbsp;&nbsp;<font color='#1E90FF'>最新固件版本:</font><b>5.2.3</b>&nbsp;&nbsp;<a href='https://blog.paldier.com/categories/firmware/' target='_blank'><em>下载链接</em></a>"
+"content4":"<font color='#1E90FF'>当前固件版本:</font><b>"+db_softcenter_["softcenter_firmware_version"]+"</b>&nbsp;&nbsp;<font color='#1E90FF'>最新固件版本:</font><b>5.2.4</b>&nbsp;&nbsp;<a href='https://blog.paldier.com/categories/firmware/' target='_blank'><em>下载链接</em></a>"
 }
